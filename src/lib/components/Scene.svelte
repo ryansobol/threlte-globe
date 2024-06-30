dbb1f8428e984a4d94bb2d0bed58e257e7c06330
Ryan Sobol <hello@ryansobol.com>
2024-06-30 01:16:15 -0700

    Refactor Globe.svelte into Scene.svelte

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
+	import type { MeshPhongMaterial } from 'three';
+
 	import { Color } from 'three';
+	import ThreeGlobe from 'three-globe';
 	import { T, useThrelte } from '@threlte/core';
 	import { OrbitControls } from '@threlte/extras';
-	import Globe from './Globe.svelte';
+	import geo from '$lib/data/geo.json';
 
 	const { scene } = useThrelte();
 
 	scene.background = new Color(0x040d21);
+
+	const globe = new ThreeGlobe();
+
+	scene.add(globe);
+
+	globe
+		.atmosphereAltitude(0.25)
+		.atmosphereColor('#3a228a')
+		.hexPolygonColor(() => 'rgba(255,255,255, 0.7)')
+		.hexPolygonMargin(0.7)
+		.hexPolygonResolution(3)
+		.hexPolygonsData(geo.features)
+		.showAtmosphere(true);
+
+	const globeMaterial = globe.globeMaterial() as MeshPhongMaterial;
+
+	globeMaterial.color = new Color(0x3a228a);
+	globeMaterial.emissive = new Color(0x220038);
+	globeMaterial.emissiveIntensity = 0.1;
+	globeMaterial.shininess = 0.7;
 </script>
 
 <T.AmbientLight args={[0xbbbbbb, 0.3]} />
@@ -27,5 +50,3 @@
 </T.PerspectiveCamera>
 
 <T.Fog args={[0x535ef3, 400, 2000]} />
-
-<Globe />
